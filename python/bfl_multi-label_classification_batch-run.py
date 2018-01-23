440811f746bcae2fe66aa6c934a4f454716c30ce
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2018-01-23 18:03:31 +0800

    Complete for batch_size and dropout.

--- python/bfl_multi-label_classification_batch-run.py
+++ python/bfl_multi-label_classification_batch-run.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import platform
+import numpy as np
 
-for i in range(10):
-    for j in range(6):
-        cmd = "./scalable_indoor_localization.py -S \"256,128,256\" -C \"64,128\" -D 0.2 -N " + str(i+1) + " --scaling " + '0.' + str(j)
+if platform.system() == 'Linux':
+    main_script = "./bfl_multi-label_classification.py"
+else:
+    main_script = "python ./bfl_multi-label_classification.py"
+default_options = " ".join([
+    "--sae_hidden_layers", "\"256,128,256\"",
+    "--classifier_hidden_layers", "\"256\""
+])
+
+for bs in [10*x for x in range(1, 6)]:
+    for dr in [0.05*x for x in range(1, 7)]:
+        var_options = " ".join([
+            "--batch_size", str(bs),
+            "--dropout", str(dr)
+        ])
+        cmd = " ".join([main_script, default_options, var_options])
         print("Executing: " + cmd)
         os.system(cmd)
         print("Completed.")
